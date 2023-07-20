2581c5fa81b0c28508237effbc02e90d04fa5f14
Eris Lund <38136789+0x5066@users.noreply.github.com>
2023-07-20 22:56:00 +0200

    progress?????

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,47 @@
+//ChatGPT assisted me with this.
+//Base code came from https://stackoverflow.com/questions/72181090/getting-pixels-of-many-different-colors-when-drawing-with-sdl-in-rust
+
 extern crate sdl2;
 
 use sdl2::event::Event;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use std::f64::consts::PI;
+use std::sync::{Arc, Mutex};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use anyhow;
 
 mod viscolors;
 
 const WINDOW_WIDTH: i32 = 75;
 const WINDOW_HEIGHT: i32 = 16;
-const SCROLL_SPEED: f64 = 0.75;
 const ZOOM: i32 = 5;
 
-fn draw_oscilloscope(canvas: &mut sdl2::render::Canvas<sdl2::video::Window>, scroll: f64, _colors: &[Color], osc_colors: &[Color]) {
-    let xs: Vec<i32> = (0..WINDOW_WIDTH).collect();
-    let ys: Vec<i32> = (0..WINDOW_WIDTH)
-        .map(|x| ((WINDOW_HEIGHT as f64 / 2.0) * (1.0 + (2.0 * PI * (x as f64 + scroll) / WINDOW_WIDTH as f64).sin())) as i32)
+fn draw_oscilloscope(
+    canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
+    _colors: &[Color],
+    osc_colors: &[Color],
+    ys: &[i32],
+) {
+
+    // Find the maximum absolute value of the audio samples for scaling
+    let max_sample = ys
+        .iter()
+        .map(|&sample| sample.abs())
+        .max()
+        .unwrap_or(1); // Avoid division by zero
+
+    let _ys_data: Vec<i32> = ys
+        .iter()
+        .map(|&sample| {
+            let scaled_sample = (sample as f32 * WINDOW_HEIGHT as f32 / 2.0 / max_sample as f32)
+                as i32;
+            scaled_sample + (WINDOW_HEIGHT-7)
+        })
         .collect();
+    println!("{_ys_data:?}");
+    let xs: Vec<i32> = (0..WINDOW_WIDTH * 4).collect();
+    let ys: Vec<i32> = _ys_data;
 
     let mut last_y = 0;
 
@@ -50,12 +75,12 @@ fn draw_oscilloscope(canvas: &mut sdl2::render::Canvas<sdl2::video::Window>, scr
     }
 }
 
-fn main() {
+fn main() -> Result<(), anyhow::Error> {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem
-        .window("rust-sdl2 demo", 400, 400)
+        .window("rust-sdl2 demo", 600, 128)
         .position_centered()
         .build()
         .unwrap();
@@ -69,7 +94,41 @@ fn main() {
     canvas.present();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut scroll: f64 = 0.0;
+
+    // The vector to store captured audio samples.
+    let ys = Arc::new(Mutex::new(Vec::<i32>::new()));
+
+    // Create a reference to ys for the callback function to store captured samples.
+    let ys_callback = ys.clone();
+
+    // Run the input stream on a separate thread.
+    std::thread::spawn(move || {
+        let host = cpal::default_host();
+        let device = host
+            .default_input_device()
+            .expect("failed to find input device");
+        let config = device
+            .default_input_config()
+            .expect("Failed to get default input config");
+
+        let err_fn = move |err| {
+            eprintln!("an error occurred on stream: {}", err);
+        };
+
+        let callback = move |data: &[f32], _: &cpal::InputCallbackInfo| {
+            let mut ys = ys_callback.lock().unwrap();
+            let ys_i32: Vec<i32> = data.iter().map(|&sample| (sample * i32::MAX as f32) as i32).collect();
+            ys.extend_from_slice(&ys_i32);            
+            //println!("{data:?}");
+        };
+
+        let stream = device.build_input_stream(&config.into(), callback, err_fn, None).unwrap();
+        stream.play().unwrap();
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        //stream.pause().unwrap();
+        //stream.into_inner();
+        //println!("Recording complete!");
+    });
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -81,13 +140,18 @@ fn main() {
 
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
-        draw_oscilloscope(&mut canvas, scroll, &viscolors, &osc_colors);
+
+        let ys = ys.lock().unwrap();
+        draw_oscilloscope(&mut canvas, &viscolors, &osc_colors, &ys);        
+        //println!("{ys:?}");
+
         canvas.present();
 
-        scroll += SCROLL_SPEED;
 
         std::thread::sleep(std::time::Duration::from_millis(16));
     }
+
+    Ok(())
 }
 
 fn osc_colors_and_peak(colors: &[Color]) -> Vec<Color> {
