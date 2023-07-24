72ce12f2cdd758b0e4942dbf6cefac30a1561770
Eris Lund <38136789+0x5066@users.noreply.github.com>
2023-07-25 00:09:14 +0200

    data is now f32, processed into u8 oscilloscope bias added

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use sdl2::rect::Rect;
 use std::sync::{Arc, Mutex};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use anyhow;
-use crossbeam_channel::{bounded, unbounded, Receiver, Sender};
+use crossbeam_channel::{unbounded, Receiver, Sender};
 use std::thread;
 
 mod viscolors;
@@ -22,17 +22,16 @@ fn draw_oscilloscope(
     canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
     _colors: &[Color],
     osc_colors: &[Color],
-    ys: &[i32],
+    ys: &[u8],
 ) {
-
     let xs: Vec<i32> = (0..WINDOW_WIDTH).collect();
-    let ys: Vec<i32> = ys.iter().map(|&sample| (sample / 8)-9).collect(); //matches winamp/wacup loudness
+    let ys: Vec<i32> = ys.iter().map(|&sample| (sample as i32 / 8) - 9).collect(); // cast to i32
 
     let mut last_y = 0;
 
-    for x in 0..WINDOW_WIDTH{
-        for y in 0..WINDOW_HEIGHT{
-            if x % 2 == 1 || y % 2 == 0{
+    for x in 0..WINDOW_WIDTH {
+        for y in 0..WINDOW_HEIGHT {
+            if x % 2 == 1 || y % 2 == 0 {
                 let rect = Rect::new(x * ZOOM, y * ZOOM, ZOOM as u32, ZOOM as u32);
                 canvas.set_draw_color(_colors[0]);
                 canvas.fill_rect(rect).unwrap();
@@ -40,15 +39,15 @@ fn draw_oscilloscope(
                 let rect = Rect::new(x * ZOOM, y * ZOOM, ZOOM as u32, ZOOM as u32);
                 canvas.set_draw_color(_colors[1]);
                 canvas.fill_rect(rect).unwrap();
-                }
             }
         }
+    }
     for (x, y) in xs.iter().zip(ys.iter()) {
         let x = *x;
         let y = *y;
 
         let x = std::cmp::min(std::cmp::max(x, 0), WINDOW_WIDTH - 1);
-        let y = std::cmp::min(std::cmp::max(-y+14, 0), WINDOW_HEIGHT - 1);
+        let y = std::cmp::min(std::cmp::max(y, 0), (WINDOW_HEIGHT - 1).try_into().unwrap());
 
         if x == 0 {
             last_y = y;
@@ -73,23 +72,27 @@ fn draw_oscilloscope(
     }
 }
 
-fn audio_stream_loop(tx: Sender<Vec<i32>>) {
+fn audio_stream_loop(tx: Sender<Vec<u8>>) {
     let host = cpal::default_host();
     let device = host.default_input_device().expect("failed to find input device");
-    //let config = device.default_input_config().expect("Failed to get default input config");
-    let streamc = cpal::StreamConfig{channels: 2, sample_rate: cpal::SampleRate(44100), buffer_size: cpal::BufferSize::Fixed(2048)};
+    let streamc = cpal::StreamConfig {
+        channels: 2,
+        sample_rate: cpal::SampleRate(44100),
+        buffer_size: cpal::BufferSize::Fixed(2048),
+    };
 
     let err_fn = move |err| {
         eprintln!("an error occurred on stream: {}", err);
     };
 
-    let callback = move |data: &[u8], _: &cpal::InputCallbackInfo| {
-        let left_channel_samples: Vec<i32> = data
+    let callback = move |data: &[f32], _: &cpal::InputCallbackInfo| {
+        // Convert f32 samples to u8 (0-255) and collect them into a Vec<u8>
+        let left_channel_samples: Vec<u8> = data
             .iter()
-            .step_by(12) // Skip every other sample (right channel)
-            //this is temporary, i need an actual buffer size
-            .map(|&sample| (sample) as i32)
+            .step_by(14) // Skip every other sample (right channel)
+            .map(|&sample| ((-sample + 1.03) * 127.5) as u8)
             .collect();
+
         // Send audio samples through the channel
         tx.send(left_channel_samples).unwrap();
     };
@@ -103,12 +106,13 @@ fn audio_stream_loop(tx: Sender<Vec<i32>>) {
     }
 }
 
+
 fn main() -> Result<(), anyhow::Error> {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem
-        .window("rust-sdl2 demo", (WINDOW_WIDTH * ZOOM) as u32, (WINDOW_HEIGHT * ZOOM) as u32)
+        .window("Winamp Mini Visualizer (in Rust)", (WINDOW_WIDTH * ZOOM) as u32, (WINDOW_HEIGHT * ZOOM) as u32)
         .position_centered()
         .build()
         .unwrap();
@@ -121,10 +125,10 @@ fn main() -> Result<(), anyhow::Error> {
     let mut event_pump = sdl_context.event_pump().unwrap();
 
     // The vector to store captured audio samples.
-    let audio_data = Arc::new(Mutex::new(Vec::<i32>::new()));
+    let audio_data = Arc::new(Mutex::new(Vec::<u8>::new()));
 
     // Create a blocking receiver to get audio samples from the audio stream loop.
-    let (tx, rx): (Sender<Vec<i32>>, Receiver<Vec<i32>>) = unbounded();
+    let (tx, rx): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = unbounded();
 
     // Start the audio stream loop in a separate thread.
     thread::spawn(move || audio_stream_loop(tx));
